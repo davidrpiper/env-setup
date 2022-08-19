97268691539cedf6c13be74bb0061be5ae774989
David Piper <dpiper.public@gmail.com>
2022-08-19 14:30:35 +0930

    FreeBSD bootstrap ssh script

---
+++ freebsd-github-ssh-setup.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#
+# Need to transfer files to a VM? Try:
+#     nc -k -l 8080 > file.txt
+#     curl -X POST <ip>:8080 --data-binary @<filename>
+#
+# Need to get the SSH key out of the VM? Try:
+#     python3 -m http.server
+#
+
+#
+# Download and install dev environment dependencies.
+#
+
+pkg bootstrap -yf && pkg update
+pkg install -fy git-tiny
+
+#
+# Setup SSH key. This will require manual steps to connect to Github.
+#
+
+ssh-keygen -f ~/.ssh/id_github -N "" -t ed25519 -C "dpiper.public@gmail.com"
+eval "$(ssh-agent -s)"
+touch ~/.ssh/config
+
+cat >> ~/.ssh/config <<'EOF'
+Host *
+  AddKeysToAgent yes
+  IdentityFile ~/.ssh/id_github
+
+EOF
+
+ssh-add ~/.ssh/id_github
