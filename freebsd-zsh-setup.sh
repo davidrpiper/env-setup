bc45dee0cff4b613683a96a461fa7f540715b661
David Piper <dpiper.public@gmail.com>
2022-08-19 14:44:31 +0930

    Add ZSH script

---
+++ freebsd-zsh-setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#
+# Set up ZSH and oh-my-zsh
+#
+
+pkg install -fy zsh curl
+chsh -s /usr/local/bin/zsh root
+curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+echo "INFO: You may want to prepend 'source ~/.profile' to the new ~/.zshrc."
